840ee18bde59e61b25b0b79a05e547bf395367a5
philippe-lacaze <philippe.lacaze@free.fr>
2018-12-12 22:48:39 +0100

    LoggerService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Logger, LoggerService} from './logger-module/logger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-module';
+
+  private logger1: Logger;
+  private logger2: Logger;
+
+  constructor(loggerService: LoggerService) {
+    this.logger1 = loggerService.getLogger('AppComponent');
+    this.logger2 = loggerService.getLogger('TotoComponent');
+    this.logger1.info('ctor');
+    this.logger2.info('ctor');
+  }
+
+  ngOnInit(): void {
+    this.logger1.log('ngOnInit', this);
+    this.logger2.log('ngOnInit', this);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgModule} from '@angular/core';
+import {LoggerService} from './logger-module/logger.service';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import {NgModule} from '@angular/core';
     // ModuleAModule.forConfig({'mock': true})
     // ModuleBModule.forConfig({'mock': true})
   ],
-  providers: [],
+  providers: [{
+    provide: LoggerService, useValue: new LoggerService(!environment.production, /.*[App|Toto].*/)
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
